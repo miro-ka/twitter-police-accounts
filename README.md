9696a912b7a413d9d5cdea75e6efcce4b8e2cc17
mKarpis <miroslav.karpis@gmail.com>
2017-01-07 01:46:47 +0100

    added Norway twitters

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # twitter-police-accounts
-List of Twitter police warnings accounts 
+
+## About
+Collection of Police Twitter accounts divided into countries. List can be used for example for a Twitter Stream bot to monitor police information and warning.
+
+
+## Contribution
+If you would like to add new Twitter account, or new country please send me a pull request.
+
+
+## Usage
+
+### Python
+
+Copy policetwitters.py file to your source directory.
+```
+import policetwitters
+
+policeNO = policetwitters.Norway
+```
---
+++ policetwitters.py
@@ -0,0 +1,18 @@
+Norway = [
+    '381502681',  # @oslopolitiops
+    '1251538416', # @Opssunnmore
+    '491140593',  # @politiopssbusk
+    '611471459',  # @RomerikePoliti
+    '424264424',  # @PolitiOstfldOPS
+    '1005826836', # @Rogalandops
+    '1090357238', # @ABpolitiops
+    '2457392205', # @politiMHPD
+    '479442225',  # @Kripos_NCIS
+    '26229790',   # @PSTnorge
+    '267691608',  # @kriseinfo_no
+    '29178136',   # @Politidir
+    '382593384',  # @Utrykningspol
+    '270364084',  # @politietostfold
+    '1073164934', # @opsenfollo
+    '55516088'    # @politietoslo
+    ]
